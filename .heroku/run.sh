95ab7e60beb65e86cb1be41d38c5de69090906e9
jlavigne <jlavigne@techso.ca>
2021-01-22 10:44:47 -0500

    Fixed run.sh

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env sh
 
-cd ../
 # Frontend first
-mkdir -p backend/src/main/resources/static
+echo 'Building the frontend'
 cd frontend
+echo 'Making folders in the backend'
+mkdir -p ../backend/src/main/resources/static
+echo 'NPM Install'
 npm install
+
+echo 'npm build'
 npm run build
 
-cp -R dist/* ../backend/src/main/resources/static/
+echo 'Moving resources to backend'
+mv ./dist/* ../backend/src/main/resources/static/
 
 # Backend then
-cd ../backend
+echo 'Building the backend'
+cd ./backend
+
+echo 'Gradle'
 ./gradlew clean assemble
