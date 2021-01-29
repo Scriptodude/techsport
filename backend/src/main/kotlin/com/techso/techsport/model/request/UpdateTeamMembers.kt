f8151bce4059cdc160f62979e1eed99c65e7ff76
jlavigne <jlavigne@techso.ca>
2021-01-29 17:07:18 -0500

    Fixed the add members

--- backend/src/main/kotlin/com/techso/techsport/model/request/UpdateTeamMembers.kt
+++ backend/src/main/kotlin/com/techso/techsport/model/request/UpdateTeamMembers.kt
@@ -1,7 +1,8 @@
 package com.techso.techsport.model.request
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 
 data class UpdateTeamMembers
 @JsonCreator
-constructor(val members: List<String>)
+constructor(@JsonProperty("members") val members: List<String>)
